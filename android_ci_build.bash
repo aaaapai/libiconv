a00bee51d6c1167f520a9c7decce89fc017d5c38
aaaapai <89151134+aaaapai@users.noreply.github.com>
2026-03-03 23:30:01 +0800

    Update android_ci_build.bash

--- android_ci_build.bash
+++ android_ci_build.bash
@@ -1,13 +1,75 @@
 #!/bin/bash
 set -e
 
-# sudo apt-get install gperf build-essential flex bison autoconf automake libtool
-cmake_build() {
-    ANDROID_ABI=$1
-    mkdir -p $ANDROID_ABI/build
-    cd $ANDROID_ABI/build
-    cmake $GITHUB_WORKSPACE -DANDROID_PLATFORM=26 -DANDROID_ABI=$ANDROID_ABI -DCMAKE_ANDROID_STL_TYPE=c++_static -DCMAKE_SYSTEM_NAME=Android -DANDROID_TOOLCHAIN=clang -DCMAKE_MAKE_PROGRAM=$ANDROID_NDK_LATEST_HOME/prebuilt/linux-x86_64/bin/make -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_LATEST_HOME/build/cmake/android.toolchain.cmake -DThreads_FOUND=ON -DCMAKE_THREAD_LIBS_INIT="-pthread" -DCMAKE_USE_PTHREADS_INIT=ON
-    cmake --build . --config Release --parallel 6
-}
+export API=26
 
-cmake_build arm64-v8a
+if   [ "$BUILD_ARCH" == "arm64" ]; then
+  export NDK_ABI=arm64-v8a NDK_TARGET=aarch64
+elif [ "$BUILD_ARCH" == "arm32" ]; then
+  export NDK_ABI=armeabi-v7a NDK_TARGET=armv7a NDK_SUFFIX=eabi
+elif [ "$BUILD_ARCH" == "x86" ]; then
+  export NDK_ABI=x86 NDK_TARGET=i686
+elif [ "$BUILD_ARCH" == "x64" ]; then
+  export NDK_ABI=x86_64 NDK_TARGET=x86_64
+fi
+
+export TARGET=$NDK_TARGET-linux-android
+export TOOLCHAIN=$ANDROID_NDK_LATEST_HOME/toolchains/llvm/prebuilt/linux-x86_64
+export CFLAGS="-flto=thin -Wno-int-conversion -fwhole-program-vtables -O3 -Wno-array-bounds -flto=thin -Wno-int-conversion -fwhole-program-vtables -Wno-ignored-attributes -Wno-array-bounds -Wno-unknown-warning-option -Wno-ignored-attributes -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0 -I$ANDROID_INCLUDE -I$ANDROID_INCLUDE/$TARGET"
+export CXXFLAGS="-O3 -Wno-array-bounds -flto=thin -Wno-int-conversion -fwhole-program-vtables -Wno-ignored-attributes -Wno-array-bounds -Wno-unknown-warning-option -Wno-ignored-attributes -flto=thin -D__GCC_HAVE_SYNC_COMPARE_AND_SWAP_4=1 -I$ANDROID_INCLUDE -I$ANDROID_INCLUDE/$TARGET -mllvm -polly -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=0"
+export EXTRA_CFLAGS="-O3 -Wno-array-bounds -flto=thin -Wno-int-conversion -fwhole-program-vtables -Wno-ignored-attributes -Wno-array-bounds -Wno-unknown-warning-option -Wno-ignored-attributes"
+export ANDROID_INCLUDE=$TOOLCHAIN/sysroot/usr/include
+export CPPFLAGS="-I$ANDROID_INCLUDE -I$ANDROID_INCLUDE/$TARGET -mllvm -polly"
+export PATH=$TOOLCHAIN/bin:$PATH
+export LDFLAGS="-L$TOOLCHAIN/sysroot/usr/lib/${TARGET}/${API} -lc++abi -lc++_static -lc -lm"
+export thecc=$TOOLCHAIN/bin/${TARGET}${API}-clang
+export thecxx=$TOOLCHAIN/bin/${TARGET}${API}-clang++
+export DLLTOOL=/usr/bin/llvm-dlltool-21
+export CXXFILT=$TOOLCHAIN/bin/llvm-cxxfilt
+export NM=$TOOLCHAIN/bin/llvm-nm
+export CC=$thecc
+export CXX=$thecxx
+export AR=$TOOLCHAIN/bin/llvm-ar
+export AS=$TOOLCHAIN/bin/llvm-as
+export LD=$TOOLCHAIN/bin/ld.lld
+export OBJCOPY=$TOOLCHAIN/bin/llvm-objcopy
+export OBJDUMP=$TOOLCHAIN/bin/llvm-objdump
+export READELF=$TOOLCHAIN/bin/llvm-readelf
+export RANLIB=$TOOLCHAIN/bin/llvm-ranlib
+export STRIP=$TOOLCHAIN/bin/llvm-strip
+export LINK=$TOOLCHAIN/bin/llvm-link
+
+for i in autoconf; do
+    echo "$i"
+    $i
+    if [ $? -ne 0 ]; then
+	echo "Error $? in $i"
+	exit 1
+    fi
+done
+
+<< EOF
+echo "./configure --enable-autogen \"$@\""
+if [ $? -ne 0 ]; then
+    echo "Error $? in ./configure"
+    exit 1
+fi
+EOF
+
+./configure \
+  --enable-autogen "$@" \
+  --host=$TARGET \
+  --prefix=${PWD}/build_android-$BUILD_ARCH \
+  --enable-doc=no \
+  || error_code=$?
+
+$ANDROID_NDK_LATEST_HOME/prebuilt/linux-x86_64/bin/make -j6
+
+if [[ "$error_code" -ne 0 ]]; then
+  echo "\n\nCONFIGURE ERROR $error_code , config.log:"
+  cat config.log
+  exit $error_code
+fi
+
+cd lib
+find ./ -name '*' -execdir ${TOOLCHAIN}/bin/llvm-strip {} \;
